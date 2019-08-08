0d5d08138ec67c8e9dcc9ccb38a6a51042a0c150
BlackBracken <me@bracken.black>
2019-08-08 16:59:46 +0900

    add libraries

--- out/production/blackjhack/Main.hs
+++ out/production/blackjhack/Main.hs
@@ -1,14 +1,16 @@
-module Main(main) where
+module Main
+  ( main
+  ) where
 
-import Blackjhack.Card
-import Blackjhack.Game
-import Blackjhack.Participant
-import Blackjhack.Participants.Player
-import Control.Monad
-import System.Random
+import           Blackjhack.Card
+import           Blackjhack.Game
+import           Blackjhack.Participant
+import           Blackjhack.Participants.Player
+import           Control.Monad
+import           System.Random
 
 main :: IO ()
 main = do
   seed <- getStdGen
   let deck = genDeck seed
-  putStrLn $ show $ fst $ prepareBoard deck [Player]
+  print (fst $ prepareBoard deck [Player])
