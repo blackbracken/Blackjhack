e7f57004c1fc967e9414bcf05aab4d0d5186380f
BlackBracken <me@bracken.black>
2019-09-10 23:37:56 +0900

    add dealing for players

--- src/Blackjhack/Game.hs
+++ src/Blackjhack/Game.hs
@@ -18,9 +18,20 @@ play deck numberOfPlayers = return $ evalState (dealInitialHand $ genSituations 
     genSituations :: Int -> [Situation]
     genSituations number = initialSituation Dealer : map (initialSituation . Player) (1 ..< number)
     dealInitialHand :: [Situation] -> State Deck [Situation]
-    dealInitialHand situations = do
-      put deck
+    dealInitialHand situations =
       forM situations $ \situation -> do
         deck <- get
-        put $ drop 2 deck
         return $ situation {hand = take 2 deck}
+    playPlayers :: [Situation] -> StateT Deck IO [Situation]
+    playPlayers [] = return []
+    playPlayers situations =
+      let dealerSituation = take 1 situations
+          playerSituations =
+            forM (drop 1 situations) $ \situation -> do
+              deck <- get
+              decision <- lift $ decideToPull situation
+              return $
+                if decision
+                  then evalState (pull 1 situation) deck
+                  else situation
+       in dealerSituation <++> playerSituations
--- src/Blackjhack/Util.hs
+++ src/Blackjhack/Util.hs
@@ -11,3 +11,12 @@ none predicate = not . any predicate
 
 (..<) :: Int -> Int -> [Int]
 (..<) start end = init [start .. end]
+
+(<++>) :: (Monad m) => [a] -> m [a] -> m [a]
+(<++>) lefts = (<$>) (\rights -> lefts ++ rights)
+
+skipMap :: Int -> (a -> a) -> [a] -> [a]
+skipMap skip predicate mapped = take skip mapped ++ map predicate (drop skip mapped)
+
+liftStateT :: (Monad m) => State a b -> StateT a m b
+liftStateT = state . runState
